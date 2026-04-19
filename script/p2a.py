89144c27d0fda2963fc498bdf0b22bfc14e763b6
dinhtrung1308 <trungduong13082000@gmail.com>
2026-04-18 22:21:35 -0400

    Update code p2a

--- script/p2a.py
+++ script/p2a.py
@@ -5,24 +5,15 @@ from geometry_msgs.msg import Quaternion
 from tf.transformations import quaternion_from_euler
 from actionlib_msgs.msg import GoalStatus
 
+# Initial pose for the robot at L1
 LOCATIONS = {
     "L1": {"x": -0.511,  "y":  13.283, "yaw":  0.00},
     "L2": {"x": 0, "y": 18.1, "yaw": 0.00},
     "L3": {"x": 12.261, "y":  18.009, "yaw": 0.00},
 }
-
-def send_goal(client, name):
-    loc  = LOCATIONS[name]
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp    = rospy.Time.now()
-    goal.target_pose.pose.position.x  = loc["x"]
-    goal.target_pose.pose.position.y  = loc["y"]
-    q = quaternion_from_euler(0, 0, loc["yaw"])
-    goal.target_pose.pose.orientation = Quaternion(*q)
-
+def print_status(gl, client, name):
     rospy.loginfo("Going to %s ...", name)
-    client.send_goal(goal)
+    client.send_goal(gl)
     client.wait_for_result()
 
     if client.get_state() == GoalStatus.SUCCEEDED:
@@ -30,12 +21,24 @@ def send_goal(client, name):
     else:
         rospy.logwarn("Failed to reach %s.", name)
 
+def send_goal(client, name):
+    loc  = LOCATIONS[name]
+    gl = MoveBaseGoal()
+    gl.target_pose.header.frame_id = "map"
+    gl.target_pose.header.stamp    = rospy.Time.now()
+    gl.target_pose.pose.position.x  = loc["x"]
+    gl.target_pose.pose.position.y  = loc["y"]
+    q = quaternion_from_euler(0, 0, loc["yaw"])
+    gl.target_pose.pose.orientation = Quaternion(*q)
+
+    print_status(gl, client, name)
+
 def main():
     rospy.init_node("simple_navigation")
     client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
     client.wait_for_server()
 
-    for stop in ["L2", "L3", "L1"]:   # starting from L1 already
+    for stop in ["L2", "L3", "L1"]:
         send_goal(client, stop)
         rospy.sleep(2.0)
 
